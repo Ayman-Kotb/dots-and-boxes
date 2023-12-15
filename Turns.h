2dbd4cd0446c0e4b0f81618ea015cf3a336a5d25
Abdelrahman <Abdelrahmansaeed@gmail.com>
2023-12-15 09:05:14 +0200

    after

--- Turns.h
+++ Turns.h
@@ -3,90 +3,88 @@
 #define RED     "\x1b[31m"
 #define YELLOW  "\x1b[33m"
 
-char letter_be_changed;
-int checked_letter;
+
+
 void player_one_turn(int MODE_B_E , char array_of_grid_of_beginners[9][13], char array_of_grid_of_experts[17][25],char Name_Player1[10])
 {
+    char letter_be_changed = '0';
+    int checked_letter = 2;
     if(MODE_B_E ==1)
     {
-      printf("%s, your turn\n",Name_Player1);
+      printf("\n%s, your turn\n",Name_Player1);
       printf("please enter the letter U want to change, %s\n",Name_Player1);
-      scanf( "%c",&letter_be_changed);
+      scanf(" %c",&letter_be_changed);
+      printf("\n");
       checked_letter = Check_Letter_In_Array_Of_Experts(letter_be_changed,array_of_grid_of_experts);
 
-      while(checked_letter==0)
+      while(checked_letter!=1)
       {
             printf("please the letter must exist in the shape\n");
             printf("please enter the letter U want to change again, %s\n",Name_Player1);
             scanf(" %c",&letter_be_changed);
+            printf("\n");
             checked_letter = Check_Letter_In_Array_Of_Experts(letter_be_changed,array_of_grid_of_experts);
       }
-      if(checked_letter==1)    grid_after_change_of_experts(letter_be_changed,array_of_grid_of_experts);         // still
+      grid_after_change_of_experts(letter_be_changed,array_of_grid_of_experts);// still 
     }
     else
     {
-      printf("%s,turn\n",Name_Player1);
+      printf("\n%s,turn\n",Name_Player1);
       printf("please enter the letter U want to change, %s\n",Name_Player1);
       scanf(" %c",&letter_be_changed);
+      printf("\n");
       checked_letter = Check_Letter_In_Array_Of_Beginners(letter_be_changed,array_of_grid_of_beginners);
-     while(checked_letter==0)
+
+      while(checked_letter!=1)
       {
             printf("please the letter must exist in the shape\n");
             printf("please enter the letter U want to change again, %s\n",Name_Player1);
             scanf(" %c",&letter_be_changed);
-            checked_letter = Check_Letter_In_Array_Of_Experts(letter_be_changed,array_of_grid_of_experts);
+            printf("\n");
+            checked_letter = Check_Letter_In_Array_Of_Beginners(letter_be_changed,array_of_grid_of_beginners);
       }
-      if(checked_letter==1)     grid_after_change_of_beginners(letter_be_changed,array_of_grid_of_beginners);       // still
-    
+      grid_after_change_of_beginners(letter_be_changed,array_of_grid_of_beginners);       // still    
     }
 }
 void player_two_turn(int MODE_B_E,char array_of_grid_of_beginners[9][13],char array_of_grid_of_experts[17][25],char Name_Player2[10])
 {
+  char letter_be_changed = '0';
+  int checked_letter = 2;
   if(MODE_B_E ==1)
     {
-      printf("%s,turn\n",Name_Player2);
+      printf("\n%s,turn\n",Name_Player2);
       printf("please enter the letter U want to change, %s\n",Name_Player2);
       scanf(" %c",&letter_be_changed);
+      printf("\n");
       checked_letter = Check_Letter_In_Array_Of_Experts(letter_be_changed,array_of_grid_of_experts);
 
-      if(checked_letter==1)
-      {
-        grid_after_change_of_experts(letter_be_changed,array_of_grid_of_experts);        // still
-      }
-      else
-      {
-        while(checked_letter==0)
+        while(checked_letter!=1)
         {
             printf("please the letter must exist in the shape\n");
             printf("please enter the letter U want to change again, %s\n",Name_Player2);
             scanf(" %c",&letter_be_changed);
+            printf("\n");
             checked_letter = Check_Letter_In_Array_Of_Experts(letter_be_changed,array_of_grid_of_experts);
         }
         grid_after_change_of_experts(letter_be_changed,array_of_grid_of_experts);           // still
-      }
     }
     else
     {
-      printf("%s,turn\n",Name_Player2);
+      printf("\n%s,turn\n",Name_Player2);
       printf("please enter the letter U want to change, %s\n",Name_Player2);
       scanf(" %c",&letter_be_changed);
+      printf("\n");
       checked_letter = Check_Letter_In_Array_Of_Beginners(letter_be_changed,array_of_grid_of_beginners);
 
-      if(checked_letter==1)
-      {
-        grid_after_change_of_beginners(letter_be_changed,array_of_grid_of_beginners);        // still
-      }
-      else
-      {
-        while(checked_letter==0)
+        while(checked_letter!=1)
         {
             printf("please the letter must exist in the shape\n");
             printf("please enter the letter U want to change again, %s\n",Name_Player2);
             scanf(" %c",&letter_be_changed);
+            printf("\n");
             checked_letter = Check_Letter_In_Array_Of_Beginners(letter_be_changed,array_of_grid_of_beginners);
         }
         grid_after_change_of_beginners(letter_be_changed,array_of_grid_of_beginners);        // still
-      }
     }
 }
 void computer_turn()       // need search
--- checks.h
+++ checks.h
@@ -2,15 +2,16 @@
 #define RED     "\x1b[31m"
 #define YELLOW  "\x1b[33m"
 
-int Found;
+
 
 int  Check_Letter_In_Array_Of_Experts(char letter_changed ,char array_of_grid_of_experts[17][25]) // check letter choosen in array
-{
-  for(int row = 0; row <17; row++)
+{ 
+  int Found = 0;
+  for(int row_c = 0; row_c <17; row_c++)
   {
-    for(int col = 0; col <25; col++)
+    for(int col_c = 0; col_c <25; col_c++)
     {
-      if(array_of_grid_of_experts[row][col]==letter_changed)
+      if(array_of_grid_of_experts[row_c][col_c]==letter_changed)
       {
         Found=1;
       }
@@ -22,19 +23,24 @@ int  Check_Letter_In_Array_Of_Experts(char letter_changed ,char array_of_grid_of
     }
     else
     {
-     return 0;
+     return 2;
     }
 }
 int Check_Letter_In_Array_Of_Beginners(char letter_changed,char array_of_grid_of_beginners[9][13]) // check letter choosen in array
 {
-  for(int row = 0; row <9; row++)
+  int Found = 0;
+  for(int row_c = 0; row_c <9; row_c++)
   {
-    for(int col = 0; col <13; col++)
+    for(int col_c = 0; col_c <13; col_c++)
     {
-      if(array_of_grid_of_beginners[row][col]==letter_changed)
+      if(array_of_grid_of_beginners[row_c][col_c]==letter_changed)
       {
         Found = 1;
       }
+    }
+    if(Found==1)
+    {
+      break;
     }
   }
   if(Found==1)
@@ -43,6 +49,6 @@ int Check_Letter_In_Array_Of_Beginners(char letter_changed,char array_of_grid_of
   }
   else
   {
-    return 0;
+    return 2;
   }
 }
